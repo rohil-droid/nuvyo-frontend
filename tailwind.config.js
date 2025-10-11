5a0c49b7ff95fbd79daa71019e9a8e48dd5a0d55
Rohil Lokuge <rohillokuge@gmail.com>
2025-10-10 21:16:11 -0400

    Optimize load speed: precompiled Tailwind + lazy hero + defer backend

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./try.html",
+    "./config.js",
+    "./nuvyo-frontend-ready/index.html",
+    "./nuvyo-frontend-ready/config.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
